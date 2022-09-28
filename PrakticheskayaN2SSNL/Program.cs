7f9fa573e37a68408d8f8e375e0f0df0cb61470e
unknxwnaix <madmaxdmitrochenko@gmail.comgit config --global user.name unknxwnaixgit config --global user.name aixgit commit -m calculgit config --global user.name aiXgit config --global user.email madmaxdmitrochenko@gmail.com>
2022-09-28 11:15:18 +0300

    ahahahahahhaa

--- PrakticheskayaN2SSNL/Program.cs
+++ PrakticheskayaN2SSNL/Program.cs
@@ -10,54 +10,15 @@ while (1 <= QuestVarI && QuestVarI <= 4)
     QuestVarI = Convert.ToInt32(Console.ReadLine());
     if (QuestVarI == 1)
     {
-        Random rnd = new Random();
-        int value, CycleVar;
-        value = rnd.Next(1, 101);
-        CycleVar = 1;
-        Console.Write("Введите предполагаемое число: ");
-        while (CycleVar != value)
-        {
-            int UserAnsw = Convert.ToInt32(Console.ReadLine());
-            CycleVar = UserAnsw;
-            if (UserAnsw < value)
-            {
-                Console.WriteLine("Больше");
-            }
-            else if (UserAnsw > value)
-            {
-                Console.WriteLine("Меньше");
-            }
-            else
-            {
-                Console.WriteLine("Ура, ты угадал");
-            }
-        }
+        Chislo();
     }
     else if (QuestVarI == 2)
     {
-        int[,] matrica = new int[10, 10];
-        for (int vertI = 1; vertI < 10; vertI++)
-            for (int gorI = 1; gorI < 10; gorI++)
-                matrica[vertI, gorI] = vertI*gorI;
-        for (int vertI = 1; vertI < 10; vertI++)
-        {
-            Console.WriteLine();
-            for (int gorI = 1; gorI < 10; gorI++)
-                Console.Write("{0,3}", matrica[vertI, gorI]);
-                Console.WriteLine("");
-        }
+        Tablica();
     }
     else if (QuestVarI == 3)
     {
-        Console.Write("Введите число для проверки: ");
-        int DelVar = Convert.ToInt32(Console.ReadLine());
-        for (int check = 1; check < (DelVar+1); check++ )
-        {
-            if (DelVar % check == 0)
-            {
-                Console.WriteLine(check);
-            }
-        }
+        Delitels();
     }
     else if (QuestVarI == 4)
     {
@@ -67,5 +28,57 @@ while (1 <= QuestVarI && QuestVarI <= 4)
     {
         Console.WriteLine("Не та команда 0_о");
         QuestVarI = 1;
+    }
+}
+
+static void Chislo()
+{
+    Random rnd = new Random();
+    int value, CycleVar;
+    value = rnd.Next(1, 101);
+    CycleVar = 1;
+    Console.Write("Введите предполагаемое число: ");
+    while (CycleVar != value)
+    {
+        int UserAnsw = Convert.ToInt32(Console.ReadLine());
+        CycleVar = UserAnsw;
+        if (UserAnsw < value)
+        {
+            Console.WriteLine("Больше");
+        }
+        else if (UserAnsw > value)
+        {
+            Console.WriteLine("Меньше");
+        }
+        else
+        {
+            Console.WriteLine("Ура, ты угадал");
+        }
+    }
+}
+static void Tablica()
+{
+    int[,] matrica = new int[10, 10];
+    for (int vertI = 1; vertI < 10; vertI++)
+        for (int gorI = 1; gorI < 10; gorI++)
+            matrica[vertI, gorI] = vertI * gorI;
+    for (int vertI = 1; vertI < 10; vertI++)
+    {
+        Console.WriteLine();
+        for (int gorI = 1; gorI < 10; gorI++)
+            Console.Write("{0,3}", matrica[vertI, gorI]);
+        Console.WriteLine("");
+    }
+}
+static void Delitels()
+{
+    Console.Write("Введите число для проверки: ");
+    int DelVar = Convert.ToInt32(Console.ReadLine());
+    for (int check = 1; check < (DelVar + 1); check++)
+    {
+        if (DelVar % check == 0)
+        {
+            Console.WriteLine(check);
+        }
     }
 }
